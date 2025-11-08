87bd68e065100882ced1dd7bf64b45157b29b50e
contyadvait <advaitthepro@outlook.com>
2025-11-08 10:57:09 +0800

    Initial Commit

---
+++ Double Pendulum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Double Pendulum
+//
+//  Created by Milind Contractor on 8/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Double Pendulum/Double_PendulumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Double_PendulumApp.swift
+//  Double Pendulum
+//
+//  Created by Milind Contractor on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Double_PendulumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
